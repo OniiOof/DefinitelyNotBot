be497a136547b8f90119c77977ecf561e28d6a1d
OniiOof <39312811+OniiOof@users.noreply.github.com>
2018-05-15 14:22:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'n>help') {
+    	message.reply('NUGGET!!');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
